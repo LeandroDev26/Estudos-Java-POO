4f9703e30ac31144d90a888ec71a723f79594d6a
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-03 14:20:56 -0300

    feat

---
+++ Exercicios-Universidade/Lista2/atividade09.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lista2atv1;
+import java.util.Scanner;
+/**
+ *
+ * @author LEAND
+ */
+public class atividade9 {
+    public static void main(String[] args) {
+         try (Scanner ler = new Scanner(System.in)) {
+             System.out.println("Digite o primeiro valor de segmento de reta :");
+              double lado1 = ler.nextDouble();
+             
+                System.out.println("Digite o segundo valor de segmento de reta :");
+                 double lado2 = ler.nextDouble();
+                 
+                   System.out.println("Digite o terceiro valor de segmento de reta :");
+                    double lado3 = ler.nextDouble();
+                    
+                     if ((lado1 <(lado2 + lado3)) && (lado2 <(lado1 + lado3)) && (lado3 <(lado1 + lado2))){
+                         
+                          if(lado1 == lado2 && lado2 == lado3){
+                         System.out.println("Eh um triangulo EQUILATERO!!");
+                     }
+                     if((lado1 == lado2 && lado3 != lado2) || (lado1 == lado3 && lado2 != lado3) || (lado3 == lado2 && lado1 != lado2)){
+                          System.out.println("Eh um triangulo ISOSCELES !!");
+                     }
+                     
+                     if((lado1 != lado2 && lado1 != lado3) && (lado1 != lado2 && lado2 != lado3) && lado1 != lado3 && lado2 != lado3 ){
+                         System.out.println("Eh um triangulo ESCALENO !!");
+                     }
+                          
+                     }else{
+                         System.out.println("Nao e um triangulo!!");
+                     }
+                     
+         }
+         
+    }
+}
