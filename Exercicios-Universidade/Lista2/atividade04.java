35a15a9e5ecc7930a546516b6931f188b67cff62
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-03 14:17:56 -0300

    feat

---
+++ Exercicios-Universidade/Lista2/atividade04.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lista2atv1;
+import java.util.Scanner;
+/**
+ *
+ * @author LEAND
+ */
+public class atividade4 {
+     public static void main(String[] args) {
+         try (Scanner ler = new Scanner(System.in)) {
+             System.out.println(" Digite um numero inteiro :");
+              int num = ler.nextInt();
+               if(num % 2 == 0){
+                   System.out.println("Esse numero eh par!!");
+               }else {
+                   System.out.println("Esse numero eh impar!!");
+               }
+             
+                     
+         }
+     }
+}
