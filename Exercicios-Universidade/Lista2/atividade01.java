9fe9b113e3c052dd8dafae76921f112e1df17c7e
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-03 14:13:55 -0300

    feat

---
+++ Exercicios-Universidade/Lista2/atividade01.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lista2atv1;
+import java.util.Scanner;
+/**
+ *
+ * @author LEAND
+ */
+public class Lista2Atv1 {
+
+    public static void main(String[] args) {
+       try (Scanner ler = new Scanner(System.in)) {
+           System.out.println("Qual e a velocidade do carro:");
+           int velocidade =ler.nextInt();
+           int multa = 0;
+           
+           if (velocidade > 80){
+               
+               multa = velocidade - 80;
+               multa *= 5;
+                System.out.println("Voce foi multado no valor de: " + multa +" R$ ");
+           }
+           
+          
+       }
+    }
+}
