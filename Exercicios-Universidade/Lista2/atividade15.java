5006aadd848fe6499a50cdd00bd353b1d28e518a
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-03 14:25:13 -0300

    feat

---
+++ Exercicios-Universidade/Lista2/atividade15.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lista2atv1;
+
+/**
+ *
+ * @author LEAND
+ */
+public class atividade15 {
+    public static void main(String[] args) {
+         int num = 100 ;
+            for (int i = 1; i < 22; i++){
+                System.out.println(num);
+               num = num -5;
+                if (i == 21 ){
+                    System.out.println("Acabou!");
+                }
+            }
+    }
+}
