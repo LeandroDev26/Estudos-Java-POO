06ceadc6579d93b17a495b359b5e2d53bdac63fa
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-03 14:19:11 -0300

    feat

---
+++ Exercicios-Universidade/Lista2/atividade06.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lista2atv1;
+import java.util.Scanner;
+/**
+ *
+ * @author LEAND
+ */
+public class atividade6 {
+     public static void main(String[] args) {
+         try (Scanner ler = new Scanner(System.in)) {
+             System.out.println("Digite o ano de seu nascimento :");
+              int ano = ler.nextInt();
+               int idade =0;
+                idade = 2025 - ano;
+                 if (idade < 18){
+                     idade = 18 - idade ;
+                     System.out.println(" Faltam " + idade + " anos para poder se alistar ");
+                     
+                 }else{
+                     idade -= 18;
+                      System.out.println(" Passaram " + idade + " anos do alistamento");
+                 }
+              
+         }
+     }
+}
