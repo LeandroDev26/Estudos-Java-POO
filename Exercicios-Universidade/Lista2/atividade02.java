c9a509b9475235882fc1c615a44f9d5c4266075c
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-04 16:52:47 -0300

    feat

--- Exercicios-Universidade/Lista2/atividade02.java
+++ Exercicios-Universidade/Lista2/atividade02.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  *
  * @author LEAND
  */
-public class atvidade2 {
+public class atividade02 {
     public static void main(String[] args) {
          try (Scanner ler = new Scanner(System.in)) {
              System.out.println("Digite om ano de seu nascimento :");
