22c7420145c21bf3fbd98813b8d12eefcc83ef53
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-04 16:56:05 -0300

    feat

--- Exercicios-Universidade/Lista2/atividade07.java
+++ Exercicios-Universidade/Lista2/atividade07.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  *
  * @author LEAND
  */
-public class atividade7 {
+public class atividade07 {
     public static void main(String[] args) {
         try (Scanner ler = new Scanner(System.in)) {
             System.out.println("Digite o seu nome :");
