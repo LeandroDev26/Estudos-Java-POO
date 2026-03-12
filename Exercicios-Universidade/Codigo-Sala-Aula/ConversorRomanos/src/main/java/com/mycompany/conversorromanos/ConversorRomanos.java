a594cd7360f69e8a072c47d8e4248978e1b21006
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-12 15:18:51 -0300

    feat

--- Exercicios-Universidade/Codigo-Sala-Aula/ConversorRomanos/src/main/java/com/mycompany/conversorromanos/ConversorRomanos.java
+++ Exercicios-Universidade/Codigo-Sala-Aula/ConversorRomanos/src/main/java/com/mycompany/conversorromanos/ConversorRomanos.java
@@ -7,18 +7,45 @@ package com.mycompany.conversorromanos;
  *
  * @author LEAND
  */
-public class ConversorRomanos {
 
-    String converter(int i) {
-        if (i == 3) {
-            return "III";
-        } else if (i == 2) {
-            return "II";
-        }else if(i == 4){
-           return "IV"; 
-        }else if( i == 5 ){
-            return "V";
+import java.util.Collections;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class ConversorRomanos {
+    // Usamos TreeMap para garantir que as chaves sejam percorridas do maior para o menor
+    private static final TreeMap<Integer, String> MAPA_ROMANO = new TreeMap<>(Collections.reverseOrder());
+
+    static {
+        MAPA_ROMANO.put(1000, "M");
+        MAPA_ROMANO.put(900, "CM");
+        MAPA_ROMANO.put(500, "D");
+        MAPA_ROMANO.put(400, "CD");
+        MAPA_ROMANO.put(100, "C");
+        MAPA_ROMANO.put(90, "XC");
+        MAPA_ROMANO.put(50, "L");
+        MAPA_ROMANO.put(40, "XL");
+        MAPA_ROMANO.put(10, "X");
+        MAPA_ROMANO.put(9, "IX");
+        MAPA_ROMANO.put(5, "V");
+        MAPA_ROMANO.put(4, "IV");
+        MAPA_ROMANO.put(1, "I");
+    }
+
+    public String converter(int numero) {
+        StringBuilder resultado = new StringBuilder();
+        
+        for (Map.Entry<Integer, String> entrada : MAPA_ROMANO.entrySet()) {
+            int valorFixo = entrada.getKey();
+            String simbolo = entrada.getValue();
+
+            // Enquanto o número for maior que o valor romano atual, "gastamos" o valor
+            while (numero >= valorFixo) {
+                resultado.append(simbolo);
+                numero -= valorFixo;
+            }
         }
-            return "I";
+        
+        return resultado.toString();
     }
 }
