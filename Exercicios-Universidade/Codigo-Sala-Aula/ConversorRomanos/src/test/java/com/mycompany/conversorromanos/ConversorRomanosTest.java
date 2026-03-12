d33c63d37e8881953ad02f17f36490e17b9809a3
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-11 21:46:47 -0300

    feat

--- Exercicios-Universidade/Codigo-Sala-Aula/ConversorRomanos/src/test/java/com/mycompany/conversorromanos/ConversorRomanosTest.java
+++ Exercicios-Universidade/Codigo-Sala-Aula/ConversorRomanos/src/test/java/com/mycompany/conversorromanos/ConversorRomanosTest.java
@@ -20,5 +20,19 @@ public class ConversorRomanosTest {
       assertEquals("I" , romano, "1 em romano é I");
       
   }
+  
+   void testeConverter2(){
+      ConversorRomanos conv = new ConversorRomanos();
+      String romano = conv.converter(2);
+      assertEquals("II" , romano, "2 em romano é II");
+      
+  }
+  
+  void testeConverter3(){
+      ConversorRomanos conv = new ConversorRomanos();
+      String romano = conv.converter(3);
+      assertEquals("III" , romano, "3 em romano é III");
+      
+  }
     
 }
